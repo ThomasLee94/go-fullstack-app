a19e5d52665cafa667411a8bc31f20218fda20e7
Thomas <thomaslee9474@gmail.com>
2019-11-13 17:12:29 -0800

    [update] db

--- handler/pokemon.go
+++ handler/pokemon.go
@@ -2,9 +2,7 @@ package pokemon
 
 import (
 	"net/http"
-	"strconv"
 
-	"golang-starter-pack/model"
 	"golang-starter-pack/utils"
 
 	"github.com/labstack/echo/v4"
@@ -20,60 +18,4 @@ func (h *Handler) GetPokemon(c echo.Context) error {
 		return c.JSON(http.StatusNotFound, utils.NotFound())
 	}
 	return c.JSON(http.StatusOK, newPokemonResponse(c, a))
-}
-
-func (h *Handler) Articles(c echo.Context) error {
-	tag := c.QueryParam("tag")
-	author := c.QueryParam("author")
-	favoritedBy := c.QueryParam("favorited")
-	offset, err := strconv.Atoi(c.QueryParam("offset"))
-	if err != nil {
-		offset = 0
-	}
-	limit, err := strconv.Atoi(c.QueryParam("limit"))
-	if err != nil {
-		limit = 20
-	}
-	var articles []model.Article
-	var count int
-	if tag != "" {
-		articles, count, err = h.articleStore.ListByTag(tag, offset, limit)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, nil)
-		}
-	} else if author != "" {
-		articles, count, err = h.articleStore.ListByAuthor(author, offset, limit)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, nil)
-		}
-	} else if favoritedBy != "" {
-		articles, count, err = h.articleStore.ListByWhoFavorited(favoritedBy, offset, limit)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, nil)
-		}
-	} else {
-		articles, count, err = h.articleStore.List(offset, limit)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, nil)
-		}
-	}
-	return c.JSON(http.StatusOK, newArticleListResponse(h.userStore, userIDFromToken(c), articles, count))
-}
-
-func (h *Handler) Feed(c echo.Context) error {
-	var articles []model.Article
-	var count int
-	offset, err := strconv.Atoi(c.QueryParam("offset"))
-	if err != nil {
-		offset = 0
-	}
-	limit, err := strconv.Atoi(c.QueryParam("limit"))
-	if err != nil {
-		limit = 20
-	}
-	articles, count, err = h.articleStore.ListFeed(userIDFromToken(c), offset, limit)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, nil)
-	}
-	return c.JSON(http.StatusOK, newArticleListResponse(h.userStore, userIDFromToken(c), articles, count))
 }
